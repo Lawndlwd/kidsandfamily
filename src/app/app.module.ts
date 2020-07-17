dd696a587d54ac610e9f604def16eeda9506a083
Levend <lwdlwd95@gmail.com>
2020-07-17 17:05:25 +0200

    getting data from the server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { MainDefultComponent } from './main/main-defult/main-defult.component';
+import { PublicationComponent } from './main/main-defult/publication/publication.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent,
+    MainDefultComponent,
+    PublicationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main-defult/publication/publication.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-publication',
+  templateUrl: './publication.component.html',
+  styleUrls: ['./publication.component.css']
+})
+export class PublicationComponent implements OnInit {
+  private url= 'https://127.0.0.1:8000/api/publications.json';
+  loadedPub= [];
+
+  constructor(private http : HttpClient) {
+   }
+
+  ngOnInit(): void {
+    this.http.get(this.url)
+    .pipe(map (responseData => {
+      let arrPub =[];
+      for(const key in responseData){
+        if (responseData.hasOwnProperty(key)){
+          arrPub.push({...responseData[key], id: key})
+        }
+      }
+      return arrPub;
+    }))
+    .subscribe(publications =>{
+      this.loadedPub = publications;
+    });
+  }
+
+}
