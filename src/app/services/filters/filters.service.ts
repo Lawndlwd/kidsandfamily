3f65655de048dfbf1d11f7a08ae71573432cab18
Quentin Picart <quentin.picart@gmail.com>
2020-07-21 16:08:38 +0200

    ajout méthode getRegions() dans filters service

--- src/app/services/filters/filters.service.ts
+++ src/app/services/filters/filters.service.ts
@@ -5,33 +5,84 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class FiltersService {
+  private url: string = 'http://localhost:8000/api/';
 
   constructor(private http: HttpClient) {}
 
   getTypes() {
-    return this.http.get('http://localhost:8000/api/types.json');
+    return this.http.get(this.url + 'types.json');
   }
 
   getPublics() {
-    return this.http.get('http://localhost:8000/api/public_cibles.json');
+    return this.http.get(this.url + 'public_cibles.json');
   }
 
   getThemes() {
-    return this.http.get('http://localhost:8000/api/themes.json');
+    return this.http.get(this.url + 'themes.json');
   }
 
   getActions() {
-    return this.http.get('http://localhost:8000/api/actions.json');
+    return this.http.get(this.url + 'actions.json');
   }
 
   getStructures() {
-    return this.http.get('http://localhost:8000/api/structures.json');
+    return this.http.get(this.url + 'structures.json');
   }
 
   getRegions() {
     return [
-      { code: 1, nom: "Bourgogne" }, 
-      { code: 2, nom: "Centre" } 
+      {
+        code: 84,
+        nom: "Auvergne-Rhône-Alpes"
+      },
+      {
+        code: 27,
+        nom: "Bourgogne-Franche-Comté"
+      },
+      {
+        code: 53,
+        nom: "Bretagne"
+      },
+      {
+        code: 24,
+        nom: "Centre-Val de Loire"
+      },
+      {
+        code: 94,
+        nom: "Corse"
+      },
+      {
+        code: 44,
+        nom: "Grand-Est"
+      },
+      {
+        code: 32,
+        nom: "Hauts-de-France"
+      },
+      {
+        code: 11,
+        nom: "Ile-de-France"
+      },
+      {
+        code: 28,
+        nom: "Normandie"
+      },
+      {
+        code: 75,
+        nom: "Nouvelle-Aquitaine"
+      },
+      {
+        code: 76,
+        nom: "Occitanie"
+      },
+      {
+        code: 52,
+        nom: "Pays de la Loire"
+      },
+      {
+        code: 93,
+        nom: "Provence-Alpes-Côte d'Azur"
+      } 
     ]
   }
 }
