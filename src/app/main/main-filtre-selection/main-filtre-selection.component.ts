8285c6bdaf299af9cf77b08afc37bdb584a23f14
team PVFTF <ahjirfaty@gmail.com>
2020-08-03 12:07:37 +0200

    reglage des details publications

--- src/app/main/main-filtre-selection/main-filtre-selection.component.ts
+++ src/app/main/main-filtre-selection/main-filtre-selection.component.ts
@@ -87,80 +87,59 @@ export class MainFiltreSelectionComponent implements OnInit {
   
 
   getCor(infos){
-    const key:string = 'dad06ede9d99985348d1d5801c524a52';
-    const limit:number= 1;
+    const key= 'dad06ede9d99985348d1d5801c524a52';
+    const limit= 1;
     return this.http.get<ResData>('http://api.positionstack.com/v1/forward?access_key='+key+'&query='+infos+'&limit=1');
 
   }
 
   ngOnInit(): void {
 
-    var macarte = L.map('map').setView([48.8587741, 2.2069771],5);
-    var markers = [];
+    const macarte = L.map('map').setView([48.8587741, 2.2069771],5);
+    const markers = [];
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
       attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
       minZoom: 1,
       maxZoom: 20
     }).addTo(macarte);
 
-    var markerClusters = L.markerClusterGroup();
-
-    // this.pubsService.getPubsNoArgment('https://127.0.0.1:8000/api/publications.json?page=1').pipe(
-    //   flatMap((res1) => this.getCor(res1.profile.country) )
-      
-    // ).subscribe((res2) => {
-    //   console.log(res2)
-    // });
+    const markerClusters = L.markerClusterGroup();
 
     this.pubsService.getPubsNoArgment('https://127.0.0.1:8000/api/publications.json?page=1').subscribe(publications =>
     {
     this.loadedPublication = publications;
-    let x = this.loadedPublication.length; 
-    // var geocoder = new google.maps.Geocoder();
+    const x = this.loadedPublication.length;
 
     for (let key = 0; key < this.loadedPublication.length; key++) {
       
-      var publication = this.loadedPublication[key];
+      const publication = this.loadedPublication[key];
       
-      let adresse = publication.profile.numVoie+' '+publication.profile.nameVoie+' '+publication.profile.codePostal 
+      const adresse = publication.profile.numVoie+' '+publication.profile.nameVoie+' '+publication.profile.codePostal 
       +' '+publication.profile.city +' '+publication.profile.country;
-      
-      // console.log(publication);
 
-      // geocoder.geocode( { 'address': adresse}, function(results, status) {
-
-      //   if (status == google.maps.GeocoderStatus.OK) {
-      //     var latitude = results[0].geometry.location.lat();
-      //     var longitude = results[0].geometry.location.lng();
-      //     // console.log(latitude, longitude);
-      //   } 
-      // }); 
-
-      var pubDetails = "<strong>"+publication.user.firstName +"</strong><br>" + publication.title + "<br>" +
+      const pubDetails = "<strong>"+publication.user.firstName +"</strong><br>" + publication.title + "<br>" +
       publication.action.actions +"<br><a  href='/publications-details/"+publication.id+"'>"+"Voir le détail</a>";
       
         console.log(pubDetails);
-        // for (let k = 0; k < pubDetails.length; k++){
+        
             
        this.getCor(adresse).subscribe(response=> {
-          // console.log(response);
+        
          if (Object.keys(response.data[0]).length !== 0) {
             var marker = L.marker([response.data[0].latitude, response.data[0].longitude]).addTo(macarte);
-
-            
+     
               marker.bindPopup(pubDetails);
               markerClusters.addLayer(marker);
-             markers.push(marker);  
+              markers.push(marker);  
              
-         }
-        
+             }
+     
          });  
-
-        // }
-         macarte.addLayer(markerClusters);   
-     }
-
+        
+         macarte.addLayer(markerClusters); 
+      }
+     
     });
-    
+  
   }
 }
