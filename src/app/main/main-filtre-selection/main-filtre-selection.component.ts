3308a8f01b6679e8b1580e1d30b3c0bf1471f303
team PVFTF <ahjirfaty@gmail.com>
2020-08-03 12:54:33 +0200

    adaptation du code mainfiltreselection

--- src/app/main/main-filtre-selection/main-filtre-selection.component.ts
+++ src/app/main/main-filtre-selection/main-filtre-selection.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit,Output } from '@angular/core';
 import { Publication } from '../main-default/publication/Publication.model';
 import { PublicationsService } from '../../services/publications/publications.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from '../../services/auth/auth.service';
-import { NgForm } from '@angular/forms';
-import { HttpClient, XhrFactory } from '@angular/common/http';
-import { Xliff2 } from '@angular/compiler';
-import { concatMap, flatMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { HttpClient} from '@angular/common/http';
 
-declare var L: any;
-declare var pubs: any;
-declare var google: any;
+declare const L: any;
 
 export class ResData {
   data:adresseData[];
 }
 
 export class adresseData {
-  administrative_area: string;
+  administrativeArea: string;
   confidence: number;
   continent: string;
   country: string;
-  country_code: string;
+  countryCode: string;
   county: string;
   label: string;
   latitude: number;
@@ -29,73 +23,53 @@ export class adresseData {
   longitude: number;
   name: string;
   neighbourhood: string;
-  number:string;
-  postal_code: string;
+  number: string;
+  postalCode: string;
   region: string;
-  region_code: string;
+  regionCode: string;
   street: string;
   type: string;
-
 }
-
-
-
-export class  pubProfileAdresse {
-  adressComplete: string;
-  city: string;
-  codePostal: string;
-  country: string;
-  id: number;
-  nameVoie: string;
-  numVoie: string;
-  sousType: [];
-  state: string;
-  type: object;
-}
-
 @Component({
   selector: 'app-main-filtre-selection',
   templateUrl: './main-filtre-selection.component.html',
   styleUrls: ['./main-filtre-selection.component.css'],
-  
 })
 
-export class MainFiltreSelectionComponent implements OnInit {
-            
-  showL=false;
-  showC=true;
+export class MainFiltreSelectionComponent implements OnInit { 
+  showL = false;
+  showC = true;
   loadedPublication: Publication[] =[];
   NumberOfPub: Number;
 
-  page: Number =1;
+  page: Number = 1;
 
    @Output() ShowList(){
-    this.showL=false;
-    this.showC=true;
+    this.showL = false;
+    this.showC = true;
   }
   @Output() ShowCard(){
-    this.showC=false;
-    this.showL=true;
+    this.showC = false;
+    this.showL = true;
   }
   constructor(
      public http: HttpClient,
-     public pubsService : PublicationsService,
+     public pubsService: PublicationsService,
      private router: Router)
      {
       this.loadedPublication = new Array<any>();
      }
-  
 
   getCor(infos){
-    const key= 'dad06ede9d99985348d1d5801c524a52';
-    const limit= 1;
+    const key = 'dad06ede9d99985348d1d5801c524a52';
+    const limit = 1;
     return this.http.get<ResData>('http://api.positionstack.com/v1/forward?access_key='+key+'&query='+infos+'&limit=1');
 
   }
 
   ngOnInit(): void {
 
-    const macarte = L.map('map').setView([48.8587741, 2.2069771],5);
+    const macarte = L.map('map').setView([48.8587741, 2.2069771], 5 );
     const markers = [];
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
       attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
@@ -111,35 +85,21 @@ export class MainFiltreSelectionComponent implements OnInit {
     const x = this.loadedPublication.length;
 
     for (let key = 0; key < this.loadedPublication.length; key++) {
-      
       const publication = this.loadedPublication[key];
-      
-      const adresse = publication.profile.numVoie+' '+publication.profile.nameVoie+' '+publication.profile.codePostal 
-      +' '+publication.profile.city +' '+publication.profile.country;
+      const adresse = publication.profile.numVoie + ' ' + publication.profile.nameVoie + ' ' + publication.profile.codePostal
+      + ' ' + publication.profile.city + ' ' + publication.profile.country;
 
-      const pubDetails = "<strong>"+publication.user.firstName +"</strong><br>" + publication.title + "<br>" +
-      publication.action.actions +"<br><a  href='/publications-details/"+publication.id+"'>"+"Voir le détail</a>";
-      
-        console.log(pubDetails);
-        
-            
+      const pubDetails = '<strong>' + publication.user.firstName + '</strong><br>' + publication.title + '<br>' +
+      publication.action.actions + '<br><a  href=\'/publications-details/' + publication.id + '\'>' + 'Voir le détail</a>';
        this.getCor(adresse).subscribe(response=> {
-        
          if (Object.keys(response.data[0]).length !== 0) {
             var marker = L.marker([response.data[0].latitude, response.data[0].longitude]).addTo(macarte);
-     
               marker.bindPopup(pubDetails);
               markerClusters.addLayer(marker);
               markers.push(marker);  
-             
-             }
-     
-         });  
-        
+             }});
          macarte.addLayer(markerClusters); 
       }
-     
     });
-  
   }
 }
